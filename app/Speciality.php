50fcc6c79aea57686b5bb788341fbbf4dafeb13c
Juan Garcia <garcia.solutions@gmail.com>
2017-08-15 16:15:27 -0500

    db

--- app/Speciality.php
+++ app/Speciality.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Speciality extends Model
 {
-    protected $fillable =['speciality_name'];
+    protected $fillable =['speciality_name','icon'];
 
     public function profiles(){
         return $this->belongsToMany('App\Profile');
