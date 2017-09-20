948bb87ab30732966a2f1d19944cd23e0c38459f
Juan Garcia <juan.garcia@primme.tech>
2017-09-20 12:58:59 -0500

    my post fix

--- app/Task.php
+++ app/Task.php
@@ -25,7 +25,7 @@ class Task extends Model
 
     public function user(){
 
-        return $this->belongsTo('App\User');
+        return $this->belongsTo('App\User')->select('id');
     }
     public function speciality(){
         
