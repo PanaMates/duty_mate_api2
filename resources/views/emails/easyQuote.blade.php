cd7d13d666036299da9d150866579e0903c310b3
Juan Garcia <juan.garcia@primme.tech>
2017-11-19 11:37:25 -0500

    email quote format

--- resources/views/emails/easyQuote.blade.php
+++ resources/views/emails/easyQuote.blade.php
@@ -13,18 +13,18 @@
 
 <table>
 <tr>
-    <th>Service Type</th><th>{{data.service_type}}</th>
-    <th>Origin </th><th>{{data.origin}}</th>
-    <th>Destiny</th><th>{{data.destiny}}</th>
+    <th>Service Type</th><th>{{$data.service_type}}</th>
+    <th>Origin </th><th>{{$data.origin}}</th>
+    <th>Destiny</th><th>{{$data.destiny}}</th>
     <th>Appointment Date</th><th>{{data.appointment_date}}</th>
-     <th>Name</th><th>{{data.name}}</th>
-    <th>email </th><th>{{data.email}}</th>
-    <th>phone</th><th>{{data.phone}}</th>
-    <th>message</th><th>{{data.message}}</th>
+     <th>Name</th><th>{{$data.name}}</th>
+    <th>email </th><th>{{$data.email}}</th>
+    <th>phone</th><th>{{$data.phone}}</th>
+    <th>message</th><th>{{$data.message}}</th>
 </tr>
 
 <tr>
-    <th>hello {{data}}</th>
+    <th>hello {{$data}}</th>
  
 </tr>
 
